f7da5c24392b396f1c42b091268bd7bed7a25cd1
Arkady Pugachev <qweeex@yandex.com>
2020-07-09 17:33:50 +0400

    Главная страница готова, осталось сделать попап обратного звонка

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -65,3 +65,79 @@ $('.arrows-right').on('click', () => {
 $('.arrows-left').on('click', () => {
     sale.slidePrev();
 });
+
+// Поиск
+
+const activeSearch = 'search-show';
+const hideBlock = '.main-hidden';
+const searchBtn = '.main__search-btn';
+const formClose = '.form-close';
+const mobSearch = '.main__search-mob';
+
+$(mobSearch).on('click', () => {
+    $('#search').addClass(activeSearch);
+});
+
+$(searchBtn).on('click', () => {
+   $(hideBlock).css('opacity', 0);
+   $('#search').addClass(activeSearch);
+});
+$(formClose).on('click', () => {
+    $(hideBlock).css('opacity', 1);
+    $('#search').removeClass(activeSearch);
+});
+
+// Миникорзина
+const cartBtn = $('.main__cart-button');
+const miniCart = $('.main__cartContent');
+const closeCart = $('.cartContent__close-btn');
+
+cartBtn.on('click', () => {
+   miniCart.removeClass('d-none');
+});
+closeCart.on('click', () => {
+    miniCart.addClass('d-none');
+});
+
+// Мобильное меню
+const mobMenu = $('.mob-menu');
+const mobBtn = $('.main__menu-mob-btn');
+const mobClose = $('.mob-menu__close-btn');
+
+mobBtn.on('click', () => {
+   mobMenu.addClass('mob-menu__active');
+});
+
+mobClose.on('click', () => {
+    mobMenu.removeClass('mob-menu__active');
+});
+
+
+// Табы на мобилке
+const tab = $('.cat-mob-link');
+
+tab.on('click', (e) => {
+    e.preventDefault();
+    let block = e.target.dataset.href;
+    tab.removeClass('link-show');
+    e.target.classList.add('link-show');
+    $('.catalog__col').removeClass('catalog__active');
+    $('.catalog__col[data-block="'+block+'"]').addClass('catalog__active');
+});
+
+
+// Scroll top
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 0) {
+        $('.up').css('opacity', 1);
+    } else {
+        $('.up').css('opacity', 0);
+    }
+});
+$('.up').click(function (e) {
+    e.preventDefault();
+    $('body,html').animate({
+        scrollTop: 0
+    }, 400);
+    return false;
+});
